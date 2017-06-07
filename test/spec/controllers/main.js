e7fb56788ae39f2f6ca542a572b819b33105bba4
yuls3 <yulishan3@gmail.com>
2017-06-07 16:09:07 +0800

    Support karma test framework

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,10 +3,9 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('basicApp'));
+  beforeEach(module('basic'));
 
-  var MainCtrl,
-    scope;
+  let MainCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
