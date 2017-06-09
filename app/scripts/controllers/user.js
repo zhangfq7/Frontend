671835cdf4641ffb28b3c48f8b612df3c0ec7986
jiasx <120084548@qq.com>
2017-06-09 12:40:13 +0800

    导航开发

---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Main Controller
+ */
+angular.module('basic').controller('UserCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+  $rootScope.tab = "user";
+}]);
