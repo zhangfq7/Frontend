13731dae4f7a1deedb17b697f57285244776f010
fooles <18730471450@163.com>
2017-06-12 14:41:28 +0800

    add home

---
+++ app/scripts/controllers/permission.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sorcerer on 2017/6/12.
+ */
+angular.module('basic')
+  .controller('PermissionCtrl',['$rootScope', '$scope', function ($rootScope, $scope) {
+    //$rootScope.tab = "service";
+  }]);
