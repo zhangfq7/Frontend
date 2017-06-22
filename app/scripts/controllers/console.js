a493f7a2bcc8224b77ceeaddc3b51af6e2e8df77
fooles <18730471450@163.com>
2017-06-22 15:33:38 +0800

    merge

--- app/scripts/controllers/console.js
+++ app/scripts/controllers/console.js
@@ -6,7 +6,14 @@
 angular.module('basic')
   .controller('ConsoleCtrl',['$rootScope', '$scope','sso', function ($rootScope, $scope,sso) {
     //$rootScope.tab = "service";
-    //sso.get({}, function (data) {
-    //  console.log('ssodata', data);
-    //})
+    sso.get(function (data) {
+      if (data['http_x_proxy_cas_loginname']) {
+        $scope.loginname=data['http_x_proxy_cas_loginname']
+        console.log('ssodata', data);
+      }
+
+
+    }, function (err) {
+      console.log('ssodata', err);
+    })
   }]);
