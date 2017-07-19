cb8ad6685f068c4f30cc758956e9f34017558c95
jingxy <953885034@qq.com>
2017-07-19 14:59:10 +0800

    用户管理需求修改

--- app/scripts/directive/services.js
+++ app/scripts/directive/services.js
@@ -268,7 +268,10 @@ angular.module('basic.services', ['ngResource'])
             namenull: false,
             emailnull: false
           };
-
+          $scope.resErr = {
+            info : '',
+            status : false
+          }
           $scope.$watch('input', function (n, o) {
             if (n === o) {
               return;
@@ -298,6 +301,9 @@ angular.module('basic.services', ['ngResource'])
             $uibModalInstance.dismiss();
           };
           $scope.isOk=false;
+            var closeConf = function(){
+              $uibModalInstance.close();
+            }
           $scope.ok = function () {
 
             if($scope.isOk===true){
@@ -324,15 +330,29 @@ angular.module('basic.services', ['ngResource'])
             if ($scope.isupdata) {
               putuser.updata($scope.input, function () {
                 $uibModalInstance.close(true);
-              }, function () {
-                  $scope.isOk=false;
+              }, function (res) {
+                if(res.data.resCodel == 4004){
+                  $scope.resErr.info = '该用户并非由您创建，您无权编辑该用户信息';
+                }else{
+                  $scope.resErr.info = '修改失败！';
+                }
+                $scope.resErr.status = true;
+                window.setTimeout(closeConf,2000);
+                $scope.isOk=false;
               });
             }else {
               console.log('111');
               user.create($scope.input, function () {
                 $uibModalInstance.close(true);
-              }, function () {
-                  $scope.isOk=false;
+              }, function (res) {
+                if(res.data.resCodel == 4003){
+                  $scope.resErr.info = '您没有权限添加用户！';
+                }else{
+                  $scope.resErr.info = '添加失败！';
+                }
+                $scope.resErr.status = true;
+                window.setTimeout(closeConf,2000);
+                $scope.isOk=false;
 
               });
             }
@@ -391,9 +411,9 @@ angular.module('basic.services', ['ngResource'])
             },function(res){
               // console.log('111',res);
               if(res.data.resCodel == 4002){
-                $scope.con = '删除失败,您不是创建者!';
+                $scope.con = '该用户并非由您创建，您无权删除该用户!';
               }else if(res.data.resCodel == 4001){
-                $scope.con = '删除失败,该用户已绑定租户!';
+                $scope.con = '该用户已被绑定角色，请解绑后再进行删除!';
               }else{
                 $scope.con = '删除失败!';
               }
