80867f0da212b48c6e54d3e634535aa570c2e21d
fooles <18730471450@163.com>
2017-07-21 18:05:27 +0800

    bug fix user

--- app/scripts/directive/services.js
+++ app/scripts/directive/services.js
@@ -70,8 +70,8 @@ angular.module('basic.services', ['ngResource'])
             config.headers.username = username;
           }
           if (config.headers) {
-            config.headers["http_x_proxy_cas_loginname"] = "jiangtong666";
-            config.headers["http_x_proxy_cas_username"] = "jiangtong666";
+            config.headers["http_x_proxy_cas_loginname"] = "admin";
+            config.headers["http_x_proxy_cas_username"] = "admin";
           }
           // if (config.headers) {
           //  config.headers["Authorization"] = "Bearer " + token;
